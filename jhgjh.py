9f8ab5cd27ab988b9e883f44dfca9ad33a678ad0
alexmoseti <alexmoseti353@gmail.com>
2025-01-30 13:07:03 +0300

    Add files via upload

---
+++ jhgjh.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Alex",
+    "age": 20,
+    "email": "alexmoseti@gmail.com",
+    "ID": 48398493,
+}
+for key, value in student.items():
+    print(key, value)
+for key in student.keys():
+    print(key)
+for value in student.values():
+    print(value)
